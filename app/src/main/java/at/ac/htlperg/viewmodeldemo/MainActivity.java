6ec2edb014fccc59e3f01b2096e1aafa455f9f83
Jakob Hoffmann <20190061@students.htl-perg.ac.at>
2023-05-11 12:59:06 +0200

    added team colors

--- app/src/main/java/at/ac/htlperg/viewmodeldemo/MainActivity.java
+++ app/src/main/java/at/ac/htlperg/viewmodeldemo/MainActivity.java
@@ -73,6 +73,44 @@ public class MainActivity extends AppCompatActivity {
             TextView teamNameView = convertView.findViewById(R.id.teamName);
             teamNameView.setText(driver.getTeam());
 
+            // Set the color of the teamNameView based on the team name
+            switch (driver.getTeam()) {
+                case "Red Bull Racing":
+                    teamNameView.setTextColor(getResources().getColor(R.color.RedBull));
+                    break;
+                case "Ferrari":
+                    teamNameView.setTextColor(getResources().getColor(R.color.Ferrari));
+                    break;
+                case "Mercedes":
+                    teamNameView.setTextColor(getResources().getColor(R.color.Mercedes));
+                    break;
+                case "Alpine":
+                    teamNameView.setTextColor(getResources().getColor(R.color.Alpine));
+                    break;
+                case "McLaren":
+                    teamNameView.setTextColor(getResources().getColor(R.color.McLaren));
+                    break;
+                case "Alfa Romeo":
+                    teamNameView.setTextColor(getResources().getColor(R.color.AlfaRomeo));
+                    break;
+                case "Aston Martin":
+                    teamNameView.setTextColor(getResources().getColor(R.color.AstonMartin));
+                    break;
+                case "Haas":
+                    teamNameView.setTextColor(getResources().getColor(R.color.Haas));
+                    break;
+                case "Alpha Tauri":
+                    teamNameView.setTextColor(getResources().getColor(R.color.AlphaTauri));
+                    break;
+                case "Williams":
+                    teamNameView.setTextColor(getResources().getColor(R.color.Williams));
+                    break;
+                default:
+                    // Use the default text color if the team name is not recognized
+                    teamNameView.setTextColor(getResources().getColor(android.R.color.black));
+                    break;
+            }
+
             ImageView driverImageView = convertView.findViewById(R.id.driverImage);
             Picasso.get().load(driver.getImage()).into(driverImageView);
 
